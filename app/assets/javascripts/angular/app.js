f6a152785d533464bb37d5a0ea7d8a17462600b6
zachnewburgh <zach.newburgh@gmail.com>
2016-09-18 10:31:02 -0400

    Added home, home.new, home.videos, home.video, and home.edit states, as well as default

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,2 +1,31 @@
 angular
-  .module('app', ['ui.router', 'ngResource']) 
+  .module('app', ['ui.router', 'ngResource'])
+  .config(function($stateProvider, $urlRouterProvider) {
+   $stateProvider
+     .state('home', {
+       url: '/',
+       templateUrl: 'home.html',
+       controller: 'HomeController as ctrl'
+     })
+     .state('home.new', {
+       url: 'new',
+       templateUrl: 'home/new.html',
+       controller: 'NewVideoController as ctrl'
+     })
+    .state('home.videos', {
+       url: 'videos',
+       templateUrl: 'home/videos.html',
+       controller: 'VideosController as ctrl'
+     })
+     .state('home.video', {
+       url: 'video/:id',
+       templateUrl: 'home/show.html',
+       controller: 'ViewVideosController as ctrl'
+     })
+    .state('home.edit', {
+     url: 'edit/:id',
+     templateUrl: 'home/edit.html',
+     controller: 'EditVideoController as ctrl'
+     });
+  $urlRouterProvider.otherwise('/');
+});
