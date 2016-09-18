7ad6fd6ae264b10e8b64d7436941583d783f5782
zachnewburgh <zach.newburgh@gmail.com>
2016-09-18 10:20:54 -0400

    Removed videos_controller and changed resources to lowercase

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults:{format: :json} do
     namespace :v1 do
-      resources :Videos, controller: 'videos_controller'
+      resources :videos
     end
   end
 end
